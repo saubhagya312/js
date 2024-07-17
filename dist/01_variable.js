4ecb9e37b9ead4840ab034aea3abcf342fde8e65
saubhagya312 <garima12.shine@gmail.com>
2024-07-17 11:54:45 +0000

    js

---
+++ dist/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 320576
+let accountEmail = "Saubhagya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 3210 // not allowed
+accountEmail = "dghdgdhdsyuhd"
+accountPassword = "3635"
+accountCity = "benguru"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ dist/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log(3+3);
+
+console.log("Saubhagya");
+
+let name = "Saubhagya"
+let age = 10
+let isLoggedIn = true;
+
+console.log(typeof undefined);
+console.log(typeof null);
