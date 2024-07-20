290d74098416767674c89dc31a9101a6ae25257e
saubhagya312 <garima12.shine@gmail.com>
2024-07-20 12:03:47 +0000

    changes in code

---
+++ dist/05_Strings.js
@@ -0,0 +1,15 @@
+const name = "Saubhagya"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const nameCode = new String('Saubhagya')
+
+// console.log(nameCode[0]);
+// console.log(nameCode.__proto__);
+// console.log(nameCode.length);
+// console.log(nameCode.toUpperCase());
+console.log(nameCode.charAt(2));
+console.log(nameCode.indexOf('S'));
