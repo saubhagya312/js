9e49f77159dea61a0758d8f58b6dc290155d4162
saubhagya312 <garima12.shine@gmail.com>
2024-07-21 17:06:08 +0530

    Update 05_Strins

--- dist/05_Strings.js
+++ dist/05_Strings.js
@@ -13,3 +13,20 @@ const nameCode = new String('Saubhagya')
 // console.log(nameCode.toUpperCase());
 console.log(nameCode.charAt(2));
 console.log(nameCode.indexOf('S'));
+const newstring = nameCode.substring(0,4)
+console.log(newstring)
+
+const newPromo = nameCode.slice(-8,4)
+console.log(newPromo);
+
+const newstring1 = "    Saubhagya.    "
+console.log(newstring1);
+console.log(newstring1.trim());
+
+const url = "https://saubhagya.com/saubhagya%20khandelwal"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('saubhagya'))
+
+console.log(nameCode.split('-'));
